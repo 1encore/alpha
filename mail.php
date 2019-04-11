7e327fa9b54fb9fe5d209dcd5292c434b6502966
MadiyarT <madiyar.wubar@gmail.com>
2019-04-12 00:46:07 +0600

    Testing mail method

--- mail.php
+++ mail.php
@@ -18,11 +18,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Set the recipient email address.
-    // FIXME: Update this to your desired email address.
     $recipient = "madiyar.torakhmet@gmail.com";
 
     // Set the email subject.
-    $subject = "New contact from $name";
+    $subject = "New contact from $name <$email>";
 
     // Build the email content.
     $email_content = "Name: $name\n";
@@ -33,8 +32,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Build the email headers.
     $email_headers = "From: $name <$email>";
 
+    $message = $message."\n From: ".$email;
+
     // Send the email.
-    if (mail($recipient, $subject, $email_content, $email_headers)) {
+    if (mail($recipient, $subject, $message)) {
         // Set a 200 (okay) response code.
         http_response_code(200);
         echo "Thank You! Your message has been sent.";
